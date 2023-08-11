07b50d71f9f67db0b646d2c4dc3d3120c2d1f9cd
something-esle <0982708461b@gmail.com>
2023-08-11 09:29:37 +0700

    upload

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import express, { Express, Response, Request } from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import Router from "./routers";
+import path from "path";
 
 const app: Express = express();
 
@@ -9,7 +11,12 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+//static file
+const staticDirectory = path.join(__dirname, "../../public");
+app.use(express.static(staticDirectory));
 //Router
+
+Router(app);
 
 //Handle Error
 
